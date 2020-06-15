0709a034f74f1d52c099a27b2bd475d00d54a9b8
Rasmus Hoffmann Birkelund <rbnielsen@gmail.com>
2020-06-15 20:56:37 +0200

    Added GetOrgs()

--- GetOrgs.py
+++ GetOrgs.py
@@ -3,74 +3,69 @@
 from pprint import pprint
 import os, time, sys
 import argparse
-#import requests
+import requests as req
 from requests import Session
 import json
 
 MAX_RETRIES = 5
 
 #api_key = os.environ['MERAKI_API_KEY']
-api_key = None
+API_KEY = None
 
 apiVersion = 'v1'
 baseURL = 'https://api.meraki.com/api/'+apiVersion
 
 class NoRebuildAuthSession(Session):
- def rebuild_auth(self, prepared_request, response):
-   '''
-   No code here means requests will always preserve the Authorization header when redirected.
-   Be careful not to leak your credentials to untrusted hosts!
-   '''
+    def rebuild_auth(self, prepared_request, response):
+        '''
+        No code here means requests will always preserve the Authorization header when redirected.
+        Be careful not to leak your credentials to untrusted hosts!
+        '''
 session = NoRebuildAuthSession()
 
-def GetOrgs():
+def GetOrgs(p_apiKey):
     orgs = None
 
     try: 
-        orgs = session.get(
-            baseURL+'/organisations',
-            headers = {
-                'Authorization': f'Bearer: {api_key}',
-                'Content-Type': 'application/json',
+        header = {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": f"Bearer {p_apiKey}",
             }
+        orgs = session.get(
+            baseURL+'/organizations',
+            headers = header
         )
-        orgs = json.loads(orgs.text)
-        pprint(orgs)
+        return orgs
     except Exception as e:
         pprint(e)
 
 # Main routine function
-def main(opts): 
-    env_key = os.environ.get('MERAKI_API_KEY', None) 
-    if env_key is None:
+def main(org_id):
+    
+    API_KEY = os.environ.get('MERAKI_API_KEY', None) 
+    if API_KEY is None:
         raise SystemExit('Please set environment variable MERAKI_API_KEY')
     else:
-        api_key = env_key
+        ...
+        #API_KEY = os.environ['MERAKI_API_KEY']
 
-    if opts is None:
-        print("Go go go")
-    elif opts in ("-o", "--get-org-id"):
-        ...
-    else:
-        ...
-        #GetOrgs()
+    if org_id is None:
+        org = GetOrgs(API_KEY)
+        print(org)
+
+
+    #print(locals())
+    
 
 def run(args):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--echo", help="Repeating")
-    parser.add_argument("-o", "--get-org-id", help='Get the Organisation ID')
-    #parser.add_argument('-g', '--get', help='Get list of networks')
-    #parser.add_argument('-p', '--post', help='Post list of networks')
+    parser.add_argument('-o', '--org-id', help='Set the Organisation ID')
+    
+    options = parser.parse_args(sys.argv[1:])
+    main(**vars(options))
 
-    arguments = parser.parse_args(sys.argv[1:])
-    if arguments.get_org_id:
-        print("haha")
-    elif arguments.echo:
-        print(arguments.echo)
-    else:
-        print("Default")
-    #main(**vars(options))
 
 if __name__ == "__main__":
     run(sys.argv[1:])
